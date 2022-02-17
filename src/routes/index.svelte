66a5e1cd2527c7020b5018d795ad443d3e05a41d
Sam Willis <37866085+samjwillis97@users.noreply.github.com>
2022-02-17 15:38:52 +1100

    Need to change the url to a environment variable

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 	import { DateInput } from 'date-picker-svelte';
 	import { onMount } from 'svelte';
 
-	let baseURL = 'http://localhost:3005'
+	let baseURL = 'http://nemweb-api:3005'
 
 	let units = [];
 	$: filteredUnits = [];
