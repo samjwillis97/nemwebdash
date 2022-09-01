bf8e107b7ca8be4e26b6a8072d3c21f9491ca729
Sam Willis <sam@williscloud.org>
2022-09-01 22:12:29 +1000

    All in on the store!

--- src/components/unitFilter.svelte
+++ src/components/unitFilter.svelte
@@ -1,25 +1,30 @@
 <script>
     import MultiFilter from '../components/multiFilter.svelte';
-    import { generationRegions, generationFuelSources, generationTechnologyTypes } from '../stores/generation/unit.js';
-    import { selectedGenerationRegions, selectedGenerationFuelSources, selectedGenerationTechnologyTypes, selectedGeneratingUnits } from '../stores/generation/selection';
+    import List from '../components/list.svelte';
+    import Textfield from '../components/textfield.svelte';
+    import { 
+        generationRegions,
+        generationFuelSources,
+        generationTechnologyTypes,
+        selectedGenerationRegions,
+        selectedGenerationFuelSources,
+        selectedGenerationTechnologyTypes,
+        selectedGeneratingUnits,
+        searchTerm
+    } from '../stores/generation/unit.js';
 
 
-    let regions = []
     let regionSelection = []
-    generationRegions.subscribe((value) => {regions = value})
     function onRegionSelection() {selectedGenerationRegions.set(regionSelection)}
 
-    let fuelSources = []
     let fuelSourceSelection = []
-    generationFuelSources.subscribe((value) => {fuelSources = value})
     function onFuelSourceSelection() {selectedGenerationFuelSources.set(fuelSourceSelection)}
 
-    let technologyTypes = []
     let technologyTypeSelection = []
-    generationTechnologyTypes.subscribe((value) => {technologyTypes = value})
     function onTechnologyTypeSelection() {selectedGenerationTechnologyTypes.set(technologyTypeSelection)}
 
-    selectedGeneratingUnits.subscribe((value) => {console.log(value)})
+    let search = ""
+    function onSearch() {searchTerm.set(search)}
 </script>
 
 <div style="display:block; width:100%; height:450px;">
@@ -27,15 +32,22 @@
     <div class="w-full pb-1 pt-2">Filters</div>
     <!-- Region -->
     <div class="px-2 py-0.5">
-        <MultiFilter items={regions} placeholder="Region" bind:selected={regionSelection} callback={onRegionSelection}></MultiFilter>
+        <MultiFilter items={$generationRegions} placeholder="Region" bind:selected={regionSelection} callback={onRegionSelection}></MultiFilter>
     </div>
     <!-- Technology Type -->
     <div class="px-2 py-0.5">
-        <MultiFilter items={technologyTypes} placeholder="Technology Type" bind:selected={technologyTypeSelection} callback={onTechnologyTypeSelection}></MultiFilter>
+        <MultiFilter items={$generationTechnologyTypes} placeholder="Technology Type" bind:selected={technologyTypeSelection} callback={onTechnologyTypeSelection}></MultiFilter>
     </div>
     <!-- Fuel Sources-->
     <div class="px-2 py-0.5">
-        <MultiFilter items={fuelSources} placeholder="Fuel Source" bind:selected={fuelSourceSelection} callback={onFuelSourceSelection}></MultiFilter>
+        <MultiFilter items={$generationFuelSources} placeholder="Fuel Source" bind:selected={fuelSourceSelection} callback={onFuelSourceSelection}></MultiFilter>
+    </div>
+    <!-- Search -->
+    <div class="p-2">
+        <Textfield placeholder="Search..." bind:value={search} onInput={onSearch}></Textfield>
+    </div>
+    <div class="h-36 px-2">
+        <List items={$selectedGeneratingUnits} titleProperty="duid" subtitleProperty="staion_name"></List>
     </div>
 </div>
 
--- src/stores/generation/unit.js
+++ src/stores/generation/unit.js
@@ -1,20 +1,179 @@
 import { writable, derived } from 'svelte/store';
-import { getUniqueItems } from '../../common/filters';
-import { selectedGeneratingUnits } from './selection';
+import { getUniqueItems, filterItems } from '../../common/filters';
 
 export const generationUnits = writable([])
+
+export const searchTerm = writable()
+export const selectedGenerationRegions = writable([])
+export const selectedGenerationFuelSources = writable([])
+export const selectedGenerationTechnologyTypes = writable([])
+
+export const selectedGeneratingUnitsNoRegions = writable([])
+export const selectedGeneratingUnitsNoFuelSources = writable([])
+export const selectedGeneratingUnitsNoTechnologyTypes = writable([])
+
+export const unitsByGenerationRegionSelection = derived(
+    [
+        generationUnits,
+        selectedGenerationRegions,
+    ],
+    ([
+        $generationUnits,
+        $selectedGenerationRegions,
+    ]) => {
+        if ($selectedGenerationRegions.length == 0) {
+            return $generationUnits
+        }
+        return filterItems(
+            $generationUnits,
+            $selectedGenerationRegions,
+            function(value) {return value.region_id}
+        )
+    },
+    []
+)
+
+export const unitsByGenerationFuelSourceSelection = derived(
+    [
+        generationUnits,
+        selectedGenerationFuelSources,
+    ],
+    ([
+        $generationUnits,
+        $selectedGenerationFuelSources,
+    ]) => {
+        if ($selectedGenerationFuelSources.length == 0) {
+            return $generationUnits
+        }
+        return filterItems(
+            $generationUnits,
+            $selectedGenerationFuelSources,
+            function(value) {return value.fuel_source}
+        )
+    },
+    []
+)
+
+export const unitsByGenerationTechnologyTypeSelection = derived(
+    [
+        generationUnits,
+        selectedGenerationTechnologyTypes,
+    ],
+    ([
+        $generationUnits,
+        $selectedGenerationTechnologyTypes,
+    ]) => {
+        if ($selectedGenerationTechnologyTypes.length == 0) {
+            return $generationUnits
+        }
+        return filterItems(
+            $generationUnits,
+            $selectedGenerationTechnologyTypes,
+            function(value) {return value.technology_type}
+        )
+    },
+    []
+)
+
+export const unitsBySearchTerm = derived(
+    [
+        generationUnits,
+        searchTerm,
+    ],
+    ([
+        $generationUnits,
+        $searchTerm,
+    ]) => {
+        if (!$searchTerm) return $generationUnits
+        return filtered = $generationUnits.filter((v) => {
+            return (
+                v.duid.toUpperCase().match(search.toUpperCase()) != null
+                || v.staion_name.toUpperCase().match(search.toUpperCase()) != null
+                )
+        })
+    },
+    []
+)
+
+export const selectedGeneratingUnits = derived(
+    [
+        unitsByGenerationRegionSelection,
+        unitsByGenerationFuelSourceSelection,
+        unitsByGenerationTechnologyTypeSelection,
+        unitsBySearchTerm
+    ],
+    ([
+        $unitsByGenerationRegionSelection,
+        $unitsByGenerationFuelSourceSelection,
+        $unitsByGenerationTechnologyTypeSelection,
+        $unitsBySearchTerm
+    ]) => {
+        let selected = []
+        let noRegions = []
+        let noFuelSources = []
+        let noTechnologyTypes = []
+
+
+        if ($unitsBySearchTerm) {
+            selected = $unitsBySearchTerm
+            noRegions = $unitsBySearchTerm
+            noFuelSources = $unitsBySearchTerm
+            noTechnologyTypes = $unitsBySearchTerm
+        }
+
+        if ($unitsByGenerationRegionSelection.length > 0) {
+            if (selected.length === 0) {
+                selected = $unitsByGenerationRegionSelection
+            } else {
+                selected = selected.filter((value) => $unitsByGenerationRegionSelection.indexOf(value) != -1)
+                noFuelSources = noFuelSources.filter((value) => $unitsByGenerationRegionSelection.indexOf(value) != -1)
+                noTechnologyTypes = noTechnologyTypes.filter((value) => $unitsByGenerationRegionSelection.indexOf(value) != -1)
+            }
+        }
+
+        if ($unitsByGenerationFuelSourceSelection.length > 0) {
+            if (selected.length === 0) {
+                selected = $unitsByGenerationFuelSourceSelection
+                noRegions = $unitsByGenerationFuelSourceSelection
+                noTechnologyTypes = $unitsByGenerationTechnologyTypeSelection
+            } else {
+                selected = selected.filter((value) => $unitsByGenerationFuelSourceSelection.indexOf(value) != -1)
+                noRegions = noRegions.filter((value) => $unitsByGenerationFuelSourceSelection.indexOf(value) != -1)
+                noTechnologyTypes = noTechnologyTypes.filter((value) => $unitsByGenerationFuelSourceSelection.indexOf(value) != -1)
+            }
+        }
+
+        if ($unitsByGenerationTechnologyTypeSelection.length > 0) {
+            if (selected.length === 0) {
+                selected = $unitsByGenerationTechnologyTypeSelection
+                noRegions = $unitsByGenerationTechnologyTypeSelection
+            } else {
+                selected = selected.filter((value) => $unitsByGenerationTechnologyTypeSelection.indexOf(value) != -1)
+                noRegions = noRegions.filter((value) => $unitsByGenerationFuelSourceSelection.indexOf(value) != -1)
+                noFuelSources = noFuelSources.filter((value) => $unitsByGenerationRegionSelection.indexOf(value) != -1)
+            }
+        }
+
+        selectedGeneratingUnitsNoRegions.set(noRegions)
+        selectedGeneratingUnitsNoFuelSources.set(noFuelSources)
+        selectedGeneratingUnitsNoTechnologyTypes.set(noTechnologyTypes)
+
+        return selected
+    },
+    []
+)
 
 export const generationRegions = derived(
     [
         generationUnits,
-        selectedGeneratingUnits,
+        selectedGeneratingUnitsNoRegions,
     ],
     ([
         $generationUnits,
-        $selectedGeneratingUnits
+        $selectedGeneratingUnitsNoRegions
     ]) => {
-        if ($selectedGeneratingUnits.length > 0) {
-            return getUniqueItems($selectedGeneratingUnits, function(value) {return value.region_id})
+        if ($selectedGeneratingUnitsNoRegions.length > 0) {
+            return getUniqueItems($selectedGeneratingUnitsNoRegions, function(value) {return value.region_id})
         } 
         return getUniqueItems($generationUnits, function(value) {return value.region_id})
     },
@@ -22,13 +181,36 @@ export const generationRegions = derived(
 )
 
 export const generationFuelSources = derived(
-    generationUnits,
-    $generationUnits => getUniqueItems($generationUnits, function(value) {return value.fuel_source}),
+    [
+        generationUnits,
+        selectedGeneratingUnitsNoFuelSources,
+    ],
+    ([
+        $generationUnits,
+        $selectedGeneratingUnitsNoFuelSources
+    ]) => {
+        if ($selectedGeneratingUnitsNoFuelSources.length > 0) {
+            return getUniqueItems($selectedGeneratingUnitsNoFuelSources, function(value) {return value.fuel_source})
+        } 
+        return getUniqueItems($generationUnits, function(value) {return value.fuel_source})
+    },
     []
 )
 
 export const generationTechnologyTypes= derived(
-    generationUnits,
-    $generationUnits => getUniqueItems($generationUnits, function(value) {return value.technology_type}),
+    [
+        generationUnits,
+        selectedGeneratingUnitsNoTechnologyTypes,
+    ],
+    ([
+        $generationUnits,
+        $selectedGeneratingUnitsNoTechnologyTypes
+    ]) => {
+        if ($selectedGeneratingUnitsNoTechnologyTypes.length > 0) {
+            return getUniqueItems($selectedGeneratingUnitsNoTechnologyTypes, function(value) {return value.technology_type})
+        } 
+        return getUniqueItems($generationUnits, function(value) {return value.technology_type})
+    },
     []
 )
+
